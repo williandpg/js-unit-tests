065680e756ad7a2444567c028e8b03ce7a1a4930
Willian Gonçalves <willian.p.g@outlook.com>
2023-05-03 18:42:26 -0300

    seação 9 finalizada

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -44,13 +44,35 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // 1. Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBe(undefined);
     // 2. Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    const objetoAryaCorreto = {
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: ['Not today', 'A girl has no name.']
+    };
+    expect(getCharacter('Arya')).toEqual(objetoAryaCorreto);
     // 3. Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    const objetoBrienneCorreto = {
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: [ 'Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.' ]
+    };
+    expect(getCharacter('Brienne')).toEqual(objetoBrienneCorreto);
     // 4. Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    const objetoMelissandreCorreto = {
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: [ 'Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.' ]
+      };
+    expect(getCharacter('Melissandre')).toEqual(objetoMelissandreCorreto);
     // 5. Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    expect(getCharacter('ArYa')).toEqual(objetoAryaCorreto);
+    expect(getCharacter('ARYA')).toEqual(objetoAryaCorreto);
+    expect(getCharacter('aRyA')).toEqual(objetoAryaCorreto);
     // 6. Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Stark')).toEqual(undefined);
   });
 });
